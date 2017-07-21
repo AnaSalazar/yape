085e30868d6107f2d64bca055348129f8fb9dd8c
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-07-21 04:03:56 -0500

    validacion de datos de usuario, pendiente de envio a la API

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,10 +7,29 @@ var habilitarContinuar = function () {
   }
 }
 
+var habilitarBotonDatosUsuario = function () {
+  var botonFormDatos = $("#form-datos");
+  var nombre = $("#nombre").val();
+  var correo = $("#correo").val()
+  var password = $("#password").val()
+  if (nombre != "" && correo != "" && password != ""){
+      botonFormDatos.removeAttr("disabled");
+      botonFormDatos.click(function () {
+        // necesito enviar los datos al API
+        window.location.href= "splash.html";
+      })
+  } else{
+       botonFormDatos.attr("disabled",true);
+  }
+}
+
 var funcionEjecutadora = function () {
   $('.carousel.carousel-slider').carousel();
   $("#numeroTelefonico").keyup(habilitarContinuar);
   $("#checkbox").click(habilitarContinuar);
+  $("#nombre").keyup(habilitarBotonDatosUsuario);
+  $("#correo").keyup(habilitarBotonDatosUsuario);
+  $("#password").keyup(habilitarBotonDatosUsuario);
 }
 
 $(document).ready(funcionEjecutadora);
